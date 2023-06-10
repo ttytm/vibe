480258dfc93b148acfd948132b4693bc49272c37
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-06-10 16:54:45 +0200

    chore: add exclamation marks to re-exported fn returns

--- src/lib.v
+++ src/lib.v
@@ -16,7 +16,7 @@ pub fn (session Session) close() {
 
 // Sends a GET request to the specified `url` and returns the response.
 pub fn (session Session) get(url string) !Response {
-	return session.get_(url)
+	return session.get_(url)!
 }
 
 // Sends a GET request to the specified `url` and returns a slice of the response content.
@@ -24,10 +24,12 @@ pub fn (session Session) get(url string) !Response {
 // The content is returned as soon as the slice reaches its `max_size` (offset from `start`)
 // - `max_size` can be `none` to return the remainder from the start.
 pub fn (session Session) get_slice(url string, start u32, size ?u32) !Response {
-	return session.get_slice_(url, start, size)
+	return session.get_slice_(url, start, size)!
 }
 
 // Sends a POST request to the specified `url` and returns the response.
 pub fn (session Session) post(url string, data string) !Response {
-	return session.post_(url, data)
+	return session.post_(url, data)!
+}
+
 }
