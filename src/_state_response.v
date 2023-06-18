6277bda1737034aaacb490005e16cc833d8ff4ce
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-06-19 01:13:50 +0200

    refactor: encapsulate download state

--- src/_state_response.v
+++ src/_state_response.v
@@ -1,7 +1,5 @@
 module vibe
 
-import os
-
 pub struct Response {
 mut:
 	pos   usize
@@ -16,22 +14,4 @@ pub mut:
 	status       Status
 	http_version string
 	body         string
-}
-
-struct FileWriter {
-mut:
-	file     os.File
-	pos      u64
-}
-
-struct ProgressWriter {
-	FileWriter
-mut:
-	size      u64
-	download Download
-}
-
-interface Download {
-	progress(u64, u64)
-	completed()
 }
