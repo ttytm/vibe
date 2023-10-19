d2423a6daf656d93bf178c7a54c749aecf00ba45
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-10-19 23:37:59 +0200

    feat: lower minimum required curl version to 7.6.4

--- curl/state/info.v
+++ curl/state/info.v
@@ -2,12 +2,13 @@
 [generated]
 module state
 
+// TODO: check commented after debian 10 EOL, June 30, 2024. Compare Ubuntu LTS 22.04 with Debian 11.
 pub enum Info {
 	activesocket              = C.CURLINFO_ACTIVESOCKET //  get the active socket
 	appconnect_time           = C.CURLINFO_APPCONNECT_TIME //  get the time until the SSL/SSH handshake is completed
 	appconnect_time_t         = C.CURLINFO_APPCONNECT_TIME_T //  time until the SSL/SSH handshake completed
-	// cainfo = C.CURLINFO_CAINFO //  get the default built-in CA certificate path
-	// capath = C.CURLINFO_CAPATH //  get the default built-in CA path string
+	// cainfo = C.CURLINFO_CAINFO //  get the default built-in CA certificate path // 7.84.0
+	// capath = C.CURLINFO_CAPATH //  get the default built-in CA path string // 7.84.0
 	certinfo                  = C.CURLINFO_CERTINFO //  get the TLS certificate chain
 	condition_unmet           = C.CURLINFO_CONDITION_UNMET //  get info on unmet time conditional or 304 HTTP response.
 	connect_time              = C.CURLINFO_CONNECT_TIME //  get the time until connect
@@ -18,7 +19,7 @@ pub enum Info {
 	content_length_upload_t   = C.CURLINFO_CONTENT_LENGTH_UPLOAD_T //  get the specified size of the upload
 	content_type              = C.CURLINFO_CONTENT_TYPE //  get Content-Type
 	cookielist                = C.CURLINFO_COOKIELIST //  get all known cookies
-	effective_method          = C.CURLINFO_EFFECTIVE_METHOD //  get the last used HTTP method
+	// effective_method          = C.CURLINFO_EFFECTIVE_METHOD //  get the last used HTTP method // 7.72.0
 	effective_url             = C.CURLINFO_EFFECTIVE_URL //  get the last used URL
 	filetime                  = C.CURLINFO_FILETIME //  get the remote time of the retrieved document
 	filetime_t                = C.CURLINFO_FILETIME_T //  get the remote time of the retrieved document
@@ -41,16 +42,16 @@ pub enum Info {
 	private                   = C.CURLINFO_PRIVATE //  get the private pointer
 	protocol                  = C.CURLINFO_PROTOCOL //  get the protocol used in the connection
 	proxyauth_avail           = C.CURLINFO_PROXYAUTH_AVAIL //  get available HTTP proxy authentication methods
-	proxy_error               = C.CURLINFO_PROXY_ERROR //  get the detailed (SOCKS) proxy error
+	// proxy_error               = C.CURLINFO_PROXY_ERROR //  get the detailed (SOCKS) proxy error // 7.74.0
 	proxy_ssl_verifyresult    = C.CURLINFO_PROXY_SSL_VERIFYRESULT //  get the result of the proxy certificate verification
 	redirect_count            = C.CURLINFO_REDIRECT_COUNT //  get the number of redirects
 	redirect_time             = C.CURLINFO_REDIRECT_TIME //  get the time for all redirection steps
 	redirect_time_t           = C.CURLINFO_REDIRECT_TIME_T //  get the time for all redirection steps
 	redirect_url              = C.CURLINFO_REDIRECT_URL //  get the URL a redirect would go to
-	referer                   = C.CURLINFO_REFERER //  get the referrer header
+	// referer                   = C.CURLINFO_REFERER //  get the referrer header // 7.76.0
 	request_size              = C.CURLINFO_REQUEST_SIZE //  get size of sent request
 	response_code             = C.CURLINFO_RESPONSE_CODE //  get the last response code
-	retry_after               = C.CURLINFO_RETRY_AFTER //  returns the Retry-After retry delay
+	// retry_after               = C.CURLINFO_RETRY_AFTER //  returns the Retry-After retry delay // 7.66.0
 	rtsp_client_cseq          = C.CURLINFO_RTSP_CLIENT_CSEQ //  get the next RTSP client CSeq
 	rtsp_cseq_recv            = C.CURLINFO_RTSP_CSEQ_RECV //  get the recently received CSeq
 	rtsp_server_cseq          = C.CURLINFO_RTSP_SERVER_CSEQ //  get the next RTSP server CSeq
